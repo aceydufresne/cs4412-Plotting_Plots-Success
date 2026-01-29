7d6ad364965528ee8e9907e4982c7eddb9ff8b3c
aceydufresne <166555240+aceydufresne@users.noreply.github.com>
2026-01-28 20:26:13 -0500

    Update dataset_prep.py

--- dataset_prep.py
+++ dataset_prep.py
@@ -1,5 +1,9 @@
-#import kagglehub
+import kagglehub
 import csv
+import re
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
 
 def printSet(path):
     movieData = []
@@ -15,9 +19,18 @@ def printSet(path):
     with open(path, mode='r', newline='', encoding='utf-8') as file:
         reader2 = csv.reader(file, delimiter=',')
         for r2 in reader2:
-            
+            print("placeholder")
+
+def createSet(titles):
+    strPath1 = "https://www.imdb.com/find/?q="
+    strPath2 = "&s=tt&ttype=ft"
+    
+    for title in titles:
+        url = strPath1 + title + strPath2
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html')
+        print(soup)
 
 if __name__ == "__main__":
     path = "rotten_tomatoes_top_movies.csv"
-    printSet(path)
-
+    #printSet(path)
