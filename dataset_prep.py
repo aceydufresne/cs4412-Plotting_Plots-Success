16d2e4ef346c39942c7c0ebd81bb3201a3d73e3a
aceydufresne <166555240+aceydufresne@users.noreply.github.com>
2026-01-28 21:16:56 -0500

    Added a webscraper for further data deconstruction
    Replaced CSV reader with pandas for more efficient data handling and updated BeautifulSoup parser.

--- dataset_prep.py
+++ dataset_prep.py
@@ -4,33 +4,30 @@ import re
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+import time
 
 def printSet(path):
     movieData = []
     columns = []
-    with open(path, mode='r', newline='', encoding='utf-8') as file:
-        reader = csv.reader(file, delimiter=',')
-        for row in reader:
-            movieData.append(row)
-    for topic in movieData[0]:
-        columns.append(topic)
-        columns[0] = '0'
-    print(columns)
-    with open(path, mode='r', newline='', encoding='utf-8') as file:
-        reader2 = csv.reader(file, delimiter=',')
-        for r2 in reader2:
-            print("placeholder")
+    #replaced the csv reader for more efficient sorting
+    df = pd.read_csv(path)
+    #print(df["title"])
+    title = df["title"]
+    return title
 
 def createSet(titles):
     strPath1 = "https://www.imdb.com/find/?q="
     strPath2 = "&s=tt&ttype=ft"
     
     for title in titles:
+        title =title.strip()
         url = strPath1 + title + strPath2
         page = requests.get(url)
-        soup = BeautifulSoup(page.text, 'html')
+        soup = BeautifulSoup(page.text, 'html.parser')
         print(soup)
+        time.sleep(1)
 
 if __name__ == "__main__":
     path = "rotten_tomatoes_top_movies.csv"
-    #printSet(path)
+    titles = printSet(path)
+    createSet(titles)
